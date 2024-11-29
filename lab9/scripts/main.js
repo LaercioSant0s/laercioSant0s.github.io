c103cb5cf1b49cf92378ba525c54be58dc5e6e78
Laércio Santos <laersantos13@hotmail.com>
2024-11-29 16:42:24 +0000

    lab 9, missing remove itm (only from the page, localstorage is removing)

--- lab9/scripts/main.js
+++ lab9/scripts/main.js
@@ -35,24 +35,17 @@ function criarProduto(produto) {
     artigo.appendChild(button);
     
     button.addEventListener('click', ()=> {
-        const objProduto = {
-            "nome": produto.title,
-            "imagem": produto.image,
-            "preco": produto.price,
-            "descricao": produto.description,
-        };
+        
+        const objProduto = JSON.stringify(produto);
 
-        let produtosSelecionados = localStorage.getItem('produtos-selecionados');
-
-        if (produtosSelecionados) {
-            produtosSelecionados = JSON.parse(produtosSelecionados);
-        } else {
-            produtosSelecionados = [];
-        }
+        let produtosSelecionados; 
+        
+        produtosSelecionados = JSON.parse(localStorage.getItem('produtos-selecionados')) || []; 
         
         produtosSelecionados.push(objProduto);
         localStorage.setItem('produtos-selecionados', JSON.stringify(produtosSelecionados));
-    })
+   
+    });
 
     return artigo;
 
@@ -71,6 +64,90 @@ function carregarProdutos(produtos) {
 
 }
 
+function removerProduto(produto) {
+
+    const selecionados = document.querySelector('.selecionados');
+    let produtos = JSON.parse(localStorage.getItem('produtos-selecionados')) || [];
+    let newList = [];
+    let toRemove;
+
+    produtos.forEach(artigo => {
+
+        if (artigo.title !== produto.title && artigo.description !== produto.description) {
+            newList.push(artigo);
+        } else {
+            toRemove = artigo;
+        }
+    
+    });
+
+    localStorage.setItem('produtos-selecionados', JSON.stringify(newList));
+
+
+    selecionados.querySelectorAll('article').forEach(article => {
+
+        const title = article.querySelector('.nomeProduto');
+        const description = article.querySelectorAll('.descricaoProduto');
+
+        if (title && description && title.textContent == toRemove.title && description.textContent == toRemove.description) {
+            selecionados.removeChild(article);
+            return;
+        } 
+
+    });
+
+
+
+
+}
+
+function criarProdutoCesto(produto) {
+
+    const artigo = document.createElement('article');
+    
+    const nome = document.createElement('p');
+    nome.className = 'nomeProduto';
+    nome.textContent = produto.title;
+
+    const imagem = document.createElement('img');
+    imagem.className = 'imgProduto';
+    imagem.setAttribute('src', produto.image);
+    imagem.setAttribute('width', '400px');
+    imagem.setAttribute('height', '500px');
+
+    const preco = document.createElement('p');
+    preco.className = 'precoProduto';
+    preco.textContent = `Custo total: ${produto.price} €`;
+
+    const descricao = document.createElement('p');
+    descricao.className = 'descricaoProduto';
+    descricao.textContent = produto.description;
+
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.textContent = `- Remover do cesto`;
+
+    artigo.appendChild(nome);
+    artigo.appendChild(imagem);
+    artigo.appendChild(preco);
+    artigo.appendChild(descricao);
+    artigo.appendChild(button);
+    
+    button.addEventListener('click', ()=> {
+        removerProduto(produto);
+    })
+
+    return artigo;
+
+}
+
+function atualizarCesto(produto) {
+
+    const cesto = document.querySelector('.selecionados');
+    const artigo = criarProdutoCesto();
+    cesto.appendChild(artigo);
+        
+}
 
 
 document.addEventListener('DOMContentLoaded', () => {
